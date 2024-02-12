748529af15c41d336f7b9a9f7db6003ded69cffa
Cerqueira <contatocerq@gmail.com>
2024-02-12 17:08:30 -0300

    Sessions routes and sessions controller has been created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :v1 do
     resources :contacts
+    resources :sessions, only:  [:create, :destroy]
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
