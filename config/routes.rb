15a40cc88c901af135279a182bdd04a43219e111
Cerqueira <contatocerq@gmail.com>
2024-02-13 11:31:11 -0300

    Sign out token authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   namespace :v1 do
     resources :contacts
-    resources :sessions, only:  [:create, :destroy]
+    resource :sessions, only:  [:create, :destroy]
   end
 
   get "up" => "rails/health#show", as: :rails_health_check
